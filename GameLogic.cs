f93d1c4dc842d19340e69e1f1d3bff53d4b40771
Vassterak <vaclavhorcicka@outlook.cz>
2022-04-04 15:21:25 +0200

    Preparation for main code

---
+++ GameLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Game_ReactionTime
+{
+    public class GameLogic
+    {
+        private void RandomButtonSelect()
+        {
+
+        }
+
+        public void NewGame(double delayMin, double delayMax, int numberOfRuns)
+        {
+
+        }
+
+        public void ButtonsPressed(int buttonID)
+        {
+
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,44 +20,59 @@ namespace WPF_Game_ReactionTime
     /// </summary>
     public partial class MainWindow : Window
     {
+        GameLogic gameLogic;
         public MainWindow()
         {
             InitializeComponent();
+            gameLogic = new GameLogic();
         }
 
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
+            double delayMin, delayMax;
+            int numberOfRuns;
+            try
+            {
+                delayMin = double.Parse(TextBoxsetTimeFrom.Text);
+                delayMax = double.Parse(TextBoxsetTimeTo.Text);
+                numberOfRuns = int.Parse(ComboBoxSelectRuns.Text);
 
+                gameLogic.NewGame(delayMin, delayMax, numberOfRuns);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Zadal jste neplatné údaje. Použijte pouze čísla s desetinou čárkou.", "Chyba");
+            }
         }
 
         private void Button0_Click(object sender, RoutedEventArgs e)
         {
-
+            gameLogic.ButtonsPressed(0);
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
-
+            gameLogic.ButtonsPressed(1);
         }
 
         private void Button2_Click(object sender, RoutedEventArgs e)
         {
-
+            gameLogic.ButtonsPressed(2);
         }
 
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
-
+            gameLogic.ButtonsPressed(3);
         }
 
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
-
+            gameLogic.ButtonsPressed(4);
         }
 
         private void Button5_Click(object sender, RoutedEventArgs e)
         {
-
+            gameLogic.ButtonsPressed(5);
         }
 
         private void ClearStats_Click(object sender, RoutedEventArgs e)
@@ -67,26 +82,30 @@ namespace WPF_Game_ReactionTime
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            string input = null;
-
             switch (e.Key)
             {
                 case Key.D:
+                    gameLogic.ButtonsPressed(0);
                     break;
 
                 case Key.F:
+                    gameLogic.ButtonsPressed(1);
                     break;
 
                 case Key.G:
+                    gameLogic.ButtonsPressed(2);
                     break;
 
                 case Key.H:
+                    gameLogic.ButtonsPressed(3);
                     break;
 
                 case Key.J:
+                    gameLogic.ButtonsPressed(4);
                     break;
 
                 case Key.K:
+                    gameLogic.ButtonsPressed(5);
                     break;
 
                 default:
